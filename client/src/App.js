050c02a29e7f9216bd6696a9d50e89267e9e66c1
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2022-06-11 17:04:59 +0530

    Remove the callback when we are using Navigate inside routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
-import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
+import CreateOrTag from './components/CreatorOrTag/CreateOrTag';
 
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
+
     <BrowserRouter>
       <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" element={() => <Redirect to="/posts" />} />
+          <Route path="/" element={<Navigate to="/posts" />} />
           <Route path="/posts" element={<Home/>} />
           <Route path="/posts/search" element={<Home/>} />
           <Route path="/posts/:id" element={<PostDetails/>} />
-          <Route path={['/creators/:name', '/tags/:name']} element ={<CreatorOrTag/>} />
-          <Route path="/auth" element={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+          <Route path="/creators/:name" element ={<CreateOrTag/>} />
+          <Route path="/tags/:name" element ={<CreateOrTag/>} />
+          <Route path="/auth" element={!user ? <Auth /> : < Navigate to="/posts"/>} />
         </Routes>
       </Container>
     </BrowserRouter>
