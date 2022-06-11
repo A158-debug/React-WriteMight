b227e7241bb93ac4fee1d61f73349a1f7d13a206
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2022-06-11 17:11:09 +0530

    Replace router with Navigate

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,20 +1,20 @@
 import {LOGIN} from '../constants/actionTypes';
 import * as  api from '../api/index.js';
 
-export const sigin = (formData, router)=>async(dispatch)=>{
+export const sigin = (formData, naviagte)=>async(dispatch)=>{
     try {
         const {data} = await api.signIn(formData);
         dispatch({type:LOGIN,data});
-        router.push('/')
+        naviagte('/')
     } catch (error) {
         console.error(error)
     }
 }
 
-export const signup = (formData,router)=>async(dispatch)=>{
+export const signup = (formData,naviagte)=>async(dispatch)=>{
     try {
         const {data} = await api.signUp(formData);
-        dispatch({type:LOGIN,data});
+        naviagte({type:LOGIN,data});
     } catch (error) {
         console.log(error)
     }
