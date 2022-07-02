eb9dabb59b81a42548bdca1af6f9d51d14e53a17
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2022-07-02 22:48:24 +0530

    add env

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/user.js'
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit:'30mb',extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb', extended: true }))
@@ -14,10 +16,9 @@ app.use(cors());
 app.use("/posts",postRoutes);
 app.use("/user",userRoutes);
 
-const CONNECTION_URL ='mongodb+srv://A158-debug:L4lamborghini@cluster0.zaeyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
 .then(()=> app.listen(PORT, ()=>console.log(`server running on port : http://localhost:${PORT}`)))
 .catch((error)=>console.log(error.message));
 
